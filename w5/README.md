d6b96ed3eb8827a0894cc91697fb1bf9413a1209
ishswar <pranay.shah@gmail.com>
2019-07-24 12:12:00 -0700

    Some fixes in Python code for netagive testing , added commnets and in Readme added some more outputs

--- w5/README.md
+++ w5/README.md
@@ -102,3 +102,66 @@ ucsc1.2019.1             18 July 2019 07:29:42 AM     swaraj_shah  28d36865206f6
               My Second file/lipsum-big.tx 6.804        18 July 2019 08:13:47 AM   STANDARD
               My Third file/lipsum.txt     3.383        18 July 2019 08:03:24 AM   STANDARD
 ```
+
+## Case when AWS credentials are not even configured on machine 
+
+```
+vagrant@amx-vbox:/vagrant/aws-cli/w5$ ./list_instances.py
+Error connecting to AWS: You must specify a region.
+The AWS CLI is not configured. Please configure it using instructions at http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+vagrant@amx-vbox:/vagrant/aws-cli/w5$
+```
+## Case when AWS credentials are not correct 
+
+```
+vagrant@amx-vbox:/vagrant/aws-cli/w5$ ./list_instances.py
+======================================  ======================================
+=========== Listing EC2 instances using boto3 ec2.instances method ===========
+======================================  ======================================
+Error connecting to AWS : An error occurred (AuthFailure) when calling the DescribeInstances operation: AWS was not able to validate the provided access credentials
+```
+
+## Case when some ec2 instances are stopped and some are running 
+
+```
+vagrant@amx-vbox:/vagrant/aws-cli/w5$ ./list_instances.py
+======================================  ======================================
+=========== Listing EC2 instances using boto3 ec2.instances method ===========
+======================================  ======================================
+Instance ID              type           image id               State        key_name     vpc id       subnet id            security group(s)         private IP address     private DNS name                             public IP address  public DNS name
+i-0271e751dd5dcbe9c      t2.micro       ami-0f2176987ee50226e  stopped      pshah2019v2  vpc-8448efe1 subnet-4a97493d      EC2_UCSC_SecurityGroup    172.31.19.30           ip-172-31-19-30.us-west-2.compute.internal   -
+i-0ee6b77f395c01c21      t2.micro       ami-0f2176987ee50226e  stopped      pshah2019v2  vpc-8448efe1 subnet-4a97493d      EC2_UCSC_SecurityGroup    172.31.16.46           ip-172-31-16-46.us-west-2.compute.internal   -
+i-05e7734787f7b377f      t2.micro       ami-0f2176987ee50226e  running      pshah2019v2  vpc-8448efe1 subnet-4a97493d      EC2_UCSC_SecurityGroup    172.31.29.254          ip-172-31-29-254.us-west-2.compute.internal  18.236.206.234     ec2-18-236-206-234.us-west-2.compute.amazonaws.com
+i-00eba519691255242      t2.micro       ami-0f2176987ee50226e  stopped      pshah2019v2  vpc-8448efe1 subnet-4a97493d      EC2_UCSC_SecurityGroup    172.31.30.83           ip-172-31-30-83.us-west-2.compute.internal   -
+======================================  ======================================
+============ Listing EC2 instances using boto3 ec2.client method =============
+======================================  ======================================
+Instance ID              type           image id               State        key_name     vpc id       subnet id            security group(s)         private IP address     private DNS name                             public IP address  public DNS name
+i-0271e751dd5dcbe9c      t2.micro       ami-0f2176987ee50226e  stopped      pshah2019v2  vpc-8448efe1 subnet-4a97493d      EC2_UCSC_SecurityGroup    172.31.19.30           ip-172-31-19-30.us-west-2.compute.internal   None
+i-0ee6b77f395c01c21      t2.micro       ami-0f2176987ee50226e  stopped      pshah2019v2  vpc-8448efe1 subnet-4a97493d      EC2_UCSC_SecurityGroup    172.31.16.46           ip-172-31-16-46.us-west-2.compute.internal   None
+i-05e7734787f7b377f      t2.micro       ami-0f2176987ee50226e  running      pshah2019v2  vpc-8448efe1 subnet-4a97493d      EC2_UCSC_SecurityGroup    172.31.29.254          ip-172-31-29-254.us-west-2.compute.internal  18.236.206.234     ec2-18-236-206-234.us-west-2.compute.amazonaws.com
+i-00eba519691255242      t2.micro       ami-0f2176987ee50226e  stopped      pshah2019v2  vpc-8448efe1 subnet-4a97493d      EC2_UCSC_SecurityGroup    172.31.30.83           ip-172-31-30-83.us-west-2.compute.internal   None
+```
+
+## Case when all ec2 instances are terminated 
+
+```
+vagrant@amx-vbox:/vagrant/aws-cli/w5$ ./list_instances.py
+======================================  ======================================
+=========== Listing EC2 instances using boto3 ec2.instances method ===========
+======================================  ======================================
+Instance ID              type           image id               State        key_name     vpc id       subnet id            security group(s)         private IP address     private DNS name                             public IP address  public DNS name
+i-0271e751dd5dcbe9c      t2.micro       ami-0f2176987ee50226e  terminated   pshah2019v2  None         None                 N/A                       N/A                    N/A                                          N/A                N/A
+i-0ee6b77f395c01c21      t2.micro       ami-0f2176987ee50226e  terminated   pshah2019v2  None         None                 N/A                       N/A                    N/A                                          N/A                N/A
+i-05e7734787f7b377f      t2.micro       ami-0f2176987ee50226e  terminated   pshah2019v2  None         None                 N/A                       N/A                    N/A                                          N/A                N/A
+i-00eba519691255242      t2.micro       ami-0f2176987ee50226e  terminated   pshah2019v2  None         None                 N/A                       N/A                    N/A                                          N/A                N/A
+======================================  ======================================
+============ Listing EC2 instances using boto3 ec2.client method =============
+======================================  ======================================
+Instance ID              type           image id               State        key_name     vpc id       subnet id            security group(s)         private IP address     private DNS name                             public IP address  public DNS name
+i-0271e751dd5dcbe9c      t2.micro       ami-0f2176987ee50226e  terminated   pshah2019v2  N/A          N/A                  N/A                       N/A                                                                 N/A
+i-0ee6b77f395c01c21      t2.micro       ami-0f2176987ee50226e  terminated   pshah2019v2  N/A          N/A                  N/A                       N/A                                                                 N/A
+i-05e7734787f7b377f      t2.micro       ami-0f2176987ee50226e  terminated   pshah2019v2  N/A          N/A                  N/A                       N/A                                                                 N/A
+i-00eba519691255242      t2.micro       ami-0f2176987ee50226e  terminated   pshah2019v2  N/A          N/A                  N/A                       N/A                                                                 N/A
+```
+
--- w5/list_instances.py
+++ w5/list_instances.py
@@ -1,52 +1,87 @@
 #!/usr/bin/env python3
 
+import sys
+
+###
+# Helper method 
+###
+
 def banner(text, ch='=', length=78):
     spaced_text = ' %s ' % text
     banner = spaced_text.center(length, ch)
     return banner
 
+
+""" Objective next two piece of code is to demonstrate that using Python Boto3 library user can query AWS e2 API ( using python)
+    First we will use boto3.resource method to get e2 instances information and print it in tabular format 
+    Second we will get same information but this time we will use boto3.client and create ec2 client to get information """
+
 import boto3
-ec2 = boto3.resource('ec2')
+from botocore.exceptions import NoRegionError
+from botocore.exceptions import ClientError
+
+try:
+   ec2 = boto3.resource('ec2')
+except NoRegionError as e:
+   print("Error connecting to AWS: " + str(e))
+   msg = "The AWS CLI is not configured."
+   msg += " Please configure it using instructions at"
+   msg += " http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html"
+   print(msg);
+   sys.exit()
+
 
 index = 0;
+
+###
+# Boto3 resource call 
+###
 
 print(banner(""))
 print(banner("Listing EC2 instances using boto3 ec2.instances method"))
 print(banner(""))
 
-for instance in ec2.instances.all():
+try:
+    for instance in ec2.instances.all():
 
-     if index == 0: 
-        print("{:<24} {:<14} {:<22} {:<12} {:<12} {:<12} {:<20} {:<25} {:<22} {:<44} {:<18} {:<12}".
-        format("Instance ID","type", "image id",  "State","key_name", "vpc id", "subnet id", "security group(s)"
-        , "private IP address", "private DNS name", "public IP address", "public DNS name"))
+         if index == 0: 
+            print("{:<24} {:<14} {:<22} {:<12} {:<12} {:<12} {:<20} {:<25} {:<22} {:<44} {:<18} {:<12}".
+            format("Instance ID","type", "image id",  "State","key_name", "vpc id", "subnet id", "security group(s)"
+            , "private IP address", "private DNS name", "public IP address", "public DNS name"))
 
-     index+=1
+         index+=1
 
-     security_groups = ""   
-     for SG in instance.security_groups:
-       security_groups += SG['GroupName'] + " "
+         security_groups = "N/A"   
+         for SG in instance.security_groups:
+           security_groups += SG['GroupName'] + " "
 
-     pri_id,pri_dns,pbub_id,pub_dns = "-","-","-","-"
-     if not instance.public_ip_address is None:
-        pbub_id = instance.public_ip_address
+         pri_id,pri_dns,pbub_id,pub_dns = "N/A","N/A","N/A","N/A"
+         if not instance.public_ip_address is None:
+            pbub_id = instance.public_ip_address
 
-     if  not instance.public_dns_name is None:
-        pub_dns = instance.public_dns_name
-     
-     if  not instance.private_ip_address is None:
-        pri_id = instance.private_ip_address
+         if  not instance.public_ip_address is None:
+            pub_dns = instance.public_dns_name
+         
+         if  not instance.private_ip_address is None:
+            pri_id = instance.private_ip_address
 
-     if not  instance.private_dns_name is None:
-        pri_dns = instance.private_dns_name
+         if not  instance.private_ip_address is None:
+            pri_dns = instance.private_dns_name
 
-     #print (instance.id,instance.state['Name'],instance.key_name,instance.image_id,instance.vpc_id,instance.subnet_id,security_groups)
-     print("{:<24} {:<14} {:<22} {:<12} {:<12} {:<12} {:<20} {:<25} {:<22} {:<44} {:<18} {:<12}".
-        format(instance.id,instance.instance_type,instance.image_id,instance.state['Name'],instance.key_name,instance.vpc_id
-            ,instance.subnet_id,security_groups[:24],pri_id,pri_dns,pbub_id,pub_dns))
+         #print (instance.id,instance.instance_type,instance.image_id,instance.state['Name'],instance.key_name,instance.vpc_id
+         #       ,instance.subnet_id,security_groups[:24],pri_id,pri_dns,pbub_id,pub_dns)
 
- #instance.terminate();
+         print("{:<24} {:<14} {:<22} {:<12} {:<12} {:<12} {:<20} {:<25} {:<22} {:<44} {:<18} {:<12}".
+            format(instance.id,instance.instance_type,instance.image_id,instance.state['Name'],instance.key_name,str(instance.vpc_id)
+                ,str(instance.subnet_id),security_groups[:24],pri_id,pri_dns,pbub_id,pub_dns))
 
+except ClientError as e:
+   print("Error connecting to AWS : " + str(e))
+   sys.exit()
+
+###
+# Boto3 client call 
+###
 
 print(banner(""))
 print(banner("Listing EC2 instances using boto3 ec2.client method"))
@@ -61,7 +96,7 @@ if len(response["Reservations"]) > 0 :
   for reservation in response["Reservations"]:
     for instance in reservation["Instances"]: 
 
-        security_groups =""
+        security_groups ="N/A"
         for SG in instance["SecurityGroups"]:
           security_groups += SG["GroupName"] + " "
 
@@ -73,15 +108,16 @@ if len(response["Reservations"]) > 0 :
          index+=1
 
         print("{:<24} {:<14} {:<22} {:<12} {:<12} {:<12} {:<20} {:<25} {:<22} {:<44} {:<18} {:<12}".
-          format(instance["InstanceId"],instance["InstanceType"],instance["ImageId"],instance["State"]["Name"],instance["KeyName"],instance["VpcId"]
-            ,instance["SubnetId"],security_groups[:24],instance["PrivateIpAddress"],instance["PrivateDnsName"],instance.get("PublicIpAddress","None"),instance["PublicDnsName"]))
+          format(instance["InstanceId"],instance["InstanceType"],instance["ImageId"],instance["State"]["Name"],instance["KeyName"],instance.get("VpcId","N/A")
+            ,instance.get("SubnetId","N/A"),security_groups[:24],instance.get("PrivateIpAddress","N/A"),instance.get("PrivateDnsName","N/A"),instance.get("PublicIpAddress","N/A"),instance.get("PublicDnsName","N/A")))
 
 else:
   print("You don't have any EC2 instances as of now")      
 
 
-
-
+""" Objective of next two code block is same as above - but this time we will use Boto3 to get information about S3 buckets 
+    First we will use boto3.resource to get S3 bucket information and content of each buckets 
+    Second we will get same information same as boto3.client """
 
 
 s3 = boto3.resource('s3')
@@ -92,28 +128,27 @@ print(banner(""))
 
 
 
-for bucket in s3.buckets.all():
+for bucket in  s3.buckets.all():
 
- print("")
- print("{:<24} {:<28} {:<22} {:<12}".
-         format("Bucket Name","CreationDate", "Owner",  "OwnerID"));
+     print("")
+     print("{:<24} {:<28} {:<22} {:<12}".
+                 format("Bucket Name","CreationDate", "Owner",  "OwnerID"));
 
- print("{:<24} {:<28} {:<12} {:<12}".
-          format(bucket.name[:23],bucket.creation_date.strftime("%d %B %Y %I:%M:%S %p"),s3.BucketAcl(bucket.name).owner["DisplayName"],s3.BucketAcl(bucket.name).owner["ID"]))
+     print("{:<24} {:<28} {:<12} {:<12}".
+                  format(bucket.name[:23],bucket.creation_date.strftime("%d %B %Y %I:%M:%S %p"),s3.BucketAcl(bucket.name).owner["DisplayName"],s3.BucketAcl(bucket.name).owner["ID"]))
 
- print("\n\tContent of bucket ["+ bucket.name+"]")
+#Print content of buckets 
+     print("\n\tContent of bucket ["+ bucket.name+"]")
 
- index=0;
+     for index,item in enumerate(bucket.objects.all()):
+      
+       if index == 0: #Print table header only once 
+         print("{:<14}{:<28} {:<12} {:<26} {:<12}".
+             format("","Object Name","Size (KB)", "ModifiedDate",  "S3 Storage Class"));
 
- for item in bucket.objects.all():
-  
-  if index == 0:
-     print("{:<14}{:<28} {:<12} {:<26} {:<12}".
-         format("","Object Name","Size (KB)", "ModifiedDate",  "S3 Storage Class"));
-  index+=1;
-  print("{:<14}{:<28} {:<12} {:<26} {:<12}".
-                format("",item.key[:28],str((item.size)/1000),item.last_modified.strftime("%d %B %Y %I:%M:%S %p"),str(item.storage_class)));
-  
+       print("{:<14}{:<28} {:<12} {:<26} {:<12}".
+                    format("",item.key[:28],str((item.size)/1000),item.last_modified.strftime("%d %B %Y %I:%M:%S %p"),str(item.storage_class)));
+      
 
 
 print(banner(""))
@@ -123,15 +158,12 @@ print(banner(""))
 s3 = boto3.client('s3')
 response = s3.list_buckets()
 
-index=0;
 
 if len(response["Buckets"]) > 0 :
     for bucket in response["Buckets"]:
-     if index == 0:
-        print("{:<24} {:<28} {:<12} {:<12}".
-         format("Bucket Name","CreationDate", "Owner",  "OwnerID"));
 
-        index+=1;
+     print("{:<24} {:<28} {:<12} {:<12}".
+         format("Bucket Name","CreationDate", "Owner",  "OwnerID"));
 
      print("{:<24} {:<28} {:<12} {:<12}".
           format(bucket["Name"][:23],bucket["CreationDate"].strftime("%d %B %Y %I:%M:%S %p"),response["Owner"]["DisplayName"],response["Owner"]["ID"]))
@@ -140,13 +172,14 @@ if len(response["Buckets"]) > 0 :
 
      print("\n\tContent of bucket ["+ bucket["Name"]+"]")
 
-     index=0;
 
-     for item in reply["Contents"]:
-          if index == 0:
+     for turn,item in enumerate(reply["Contents"]):
+          if turn == 0:
              print("{:<14}{:<28} {:<12} {:<26} {:<12}".
                  format("","Object Name","Size (KB)", "ModifiedDate",  "S3 Storage Class"));
-          index+=1;
+
           print("{:<14}{:<28} {:<12} {:<26} {:<12}".
                 format("",item["Key"][:28],str(item["Size"]/1000),item["LastModified"].strftime("%d %B %Y %I:%M:%S %p"),str(item["StorageClass"])));
+else:
+    print("You seems to not have any S3 buckets")
  
